2a5192b9c0a26d2c2014ccfd0c5a75aaaff5b548
Benjamin Smith <ben.smith.developer@gmail.com>
2023-07-27 13:12:31 -0700

    auto commit dist buil

--- src/operations/kebabcase.ts
+++ src/operations/kebabcase.ts
@@ -1,16 +1,5 @@
-import * as core from "@actions/core";
 export function kebabcase(str: string): string {
-  const string1 = str.replace(/([a-z])([A-Z])/g, "$1-$2")
-  console.log("string1: " + string1)
-  core.debug("string1: " + string1)
-  const string2 = string1.replace(/[\s_/]+/g, '-')
-  console.debug("string2: " + string2)
-  core.info("string2: " + string2)
-  const string3 = string2.toLowerCase()
-  console.debug("string3: " + string3)
-  
   return str.replace(/([a-z])([A-Z])/g, "$1-$2")
-    //eslint-disable-next-line no-useless-escape
     .replace(/[\s_/]+/g, '-')
-    .toLowerCase().replace('/', '-');
+    .toLowerCase();
 }
