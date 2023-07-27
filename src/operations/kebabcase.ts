9aa80fd7a2e5f8f32d21f0596b1912f551956e39
Benjamin Smith <ben.smith.developer@gmail.com>
2023-07-27 12:55:28 -0700

    ignore eslint

--- src/operations/kebabcase.ts
+++ src/operations/kebabcase.ts
@@ -1,5 +1,6 @@
 export function kebabcase(str: string): string {
   return str.replace(/([a-z])([A-Z])/g, "$1-$2")
+    //eslint-disable-next-line no-useless-escape
     .replace(/[\s_\/]+/g, '-')
     .toLowerCase().replace('/', '-');
 }
